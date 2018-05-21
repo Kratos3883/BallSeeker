0c86641fffc66db28916d3498ceca2f1d4e50199
Samira Sanchez <samirasancheze@gmail.com>
2018-05-21 16:21:09 -0400

    Se modificó el modelo de los motores, se le añadió el factor Bm en la función de transferencia

--- Motores/Modelado/Modelado_motores.m
+++ Motores/Modelado/Modelado_motores.m
@@ -3,12 +3,13 @@ K1=0.29;
 R1=1.11;
 L1=286e-6;
 J1=19.3e-6;
+B1=0.02;
 
 Num1=[K1];
-Den1=[L1*J1 R1*J1 K1^2];
-TF1= tf(Num1, Den1);
+Den1=[L1*J1 R1*J1+B1*L1 B1*R1+K1^2];
+TF_R_N= tf(Num1, Den1);
 figure(1)
-plot(step(TF1))
+plot(step(TF_R_N))
 
 %MOTOR VERDE/BLANCO
 
@@ -16,9 +17,10 @@ K2=0.29;
 R2=1.09;
 L2=339e-6;
 J2=19.3e-6;
+B2=0.02;
 
 Num2=[K2];
-Den2=[L2*J2 R2*J2 K2^2];
-TF2= tf(Num2, Den2);
+Den2=[L2*J2 (R2*J2+L2*B2) B2*R2+K2^2];
+TF_V_B= tf(Num2, Den2);
 figure(2)
-plot(step(TF2))
+plot(step(TF_V_B))
